05bdd35a38060b3e91370b9689b4e9af0e7fa60a
Cristi <cristi@gmail.com>
2026-06-07 18:05:44 +0100

    build(android): fix cross-drive compile crash and configure release debugSymbolLevel

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -56,6 +56,9 @@ android {
                 signingConfigs.getByName("debug")
             }
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            ndk {
+                debugSymbolLevel = "none"
+            }
         }
     }
 }
